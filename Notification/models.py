9632ced328db45405ac094b3fd959522aa1f0a29
akshaykkl <akkukkl10@gmail.com>
2024-02-14 10:47:09 +0530

    Second Commit

--- Notification/models.py
+++ Notification/models.py
@@ -8,23 +8,31 @@ class Department(models.Model):
     def  __str__(self):
         return self.dept_name
 
+class Programme(models.Model):
+    pgm_name = models.CharField(max_length=50)
+    grad_level = models.CharField(max_length=10)
+    dept_id = models.ForeignKey(Department, on_delete=models.CASCADE, default=1)
+    def __str__(self):
+        return self.pgm_name
+
 class Student(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     admn_no = models.IntegerField()
     name = models.CharField(max_length=30)
     year_of_admission = models.IntegerField()
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, default=1)
+    pgm_id = models.ForeignKey(Programme, on_delete=models.CASCADE, default=1)
     admission_no = models.IntegerField()
     current_sem = models.IntegerField()
     status = models.TextField()
+    def __str__(self):
+        return self.name
 
 class Teacher(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     teacher_id = models.IntegerField()
     name = models.CharField(max_length=50)
     designation = models.CharField(max_length=20)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, default=1)
+    dept_id = models.ForeignKey(Department, on_delete=models.CASCADE, default=1)
     hod = models.BooleanField()
     def  __str__(self):
         return self.name + self.department.dept_name
-
--- Notification/views.py
+++ Notification/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     current_user = request.user
     if current_user.is_authenticated and current_user.groups.filter(name= 'Teacher').exists():
-        return render(request, 'Notification/home.html')
+        return render(request, 'Notification/teacher.html')
     else:
         return HttpResponse('hi')
 
